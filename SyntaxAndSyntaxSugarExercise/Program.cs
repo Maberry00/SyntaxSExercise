18775b6275fa86d1b6ed9565421b139b659577b0
Maberry00 <maberry30@gmail.com>
2024-01-16 15:09:54 -0600

    synt

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = "";
+
+            response = (answer < 9) ? $" {answer} is less than nine":
+                $"s{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+
+
+
         }
     }
 }
